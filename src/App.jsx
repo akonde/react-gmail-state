170a17f315add4c0489114f0d668e78940bfe346
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-07-02 19:03:29 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import Header from './components/Header'
-import initialEmails from './data/emails'
+import { useState } from "react";
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  // console.log(initialEmails);
+  const [emails, setEmails] = useState(initialEmails);
+
+  function toggleRead(index){
+    const updatedEmails = emails.map((email, i) => {
+      if (i === index) {
+        return { ...email, read: !email.read };
+      }
+      return email;
+    });
+    setEmails(updatedEmails);
+  }
+
+  function toggleStar(index){
+    const updatedEmails = emails.map((email, i) => {
+      if (i === index) {
+        return { ...email, read: !email.starred};
+      }
+      return email;
+    });
+    setEmails(updatedEmails);
+  }
 
   return (
     <div className="app">
@@ -14,17 +36,19 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+          onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emails.length}</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+          // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">
+              {initialEmails.filter((email) => email.starred).length}
+            </span>
           </li>
 
           <li className="item toggle">
@@ -33,14 +57,29 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+            onChange={() => {toggleRead(index)}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {emails.map((email, index) => {
+          return (
+            <li key={index} className="email">
+              <div className="select">
+                <input className="select-checkbox" type="checkbox" />
+              </div>
+              <div className="star">
+                <input className="star-checkbox" type="checkbox" />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          )
+        })}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
